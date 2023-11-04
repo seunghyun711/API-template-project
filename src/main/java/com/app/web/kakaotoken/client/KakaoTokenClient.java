8b22eef91f60ab6d27ec5f9ae386923d13d091d1
seunghyun711 <seunghyun711@gmail.com>
2023-11-04 13:49:51 +0900

    kakao login - Kakao Token 발급

---
+++ src/main/java/com/app/web/kakaotoken/client/KakaoTokenClient.java
@@ -0,0 +1,14 @@
+package com.app.web.kakaotoken.client;
+
+import com.app.web.kakaotoken.dto.KakaoTokenDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(url = "https://kauth.kakao.com", name = "KakaoTokenClient")
+public interface KakaoTokenClient {
+    @PostMapping(value = "/oauth/token", consumes = "application/json")
+    KakaoTokenDto.Response requestKakaoToken(@RequestHeader("Content-Type") String contentType,
+                                             @SpringQueryMap KakaoTokenDto.Request request);
+}
