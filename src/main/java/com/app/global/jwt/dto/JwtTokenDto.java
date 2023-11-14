1075b9f4be935e9baa5dbf057443cfeda62f3f1a
seunghyun711 <seunghyun711@gmail.com>
2023-11-14 23:42:52 +0900

    토큰 만료시간 한국 시간으로 설정

--- src/main/java/com/app/global/jwt/dto/JwtTokenDto.java
+++ src/main/java/com/app/global/jwt/dto/JwtTokenDto.java
@@ -1,5 +1,6 @@
 package com.app.global.jwt.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.util.Date;
@@ -12,7 +13,9 @@ import java.util.Date;
 public class JwtTokenDto {
     private String grantType; // grantType
     private String accessToken; // 액세스 토큰
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul") // 한국 시간에 맞게 만료시간 형식 설정
     private Date accessTokenExpireTime; // 액세스 토큰 만료 시간
     private String refreshToken; // 리프레시 토큰
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private Date refreshTokenExpireTime; // 리프레시 토큰 만료 시간
 }
