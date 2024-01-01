63bbd111437504f44a6e7117486ffdac7423dc72
seunghyun711 <seunghyun711@gmail.com>
2024-01-02 01:04:27 +0900

    Swagger를 이용한 API 문서화 - Logout

--- src/main/java/com/app/api/logout/controller/LogoutController.java
+++ src/main/java/com/app/api/logout/controller/LogoutController.java
@@ -2,6 +2,8 @@ package com.app.api.logout.controller;
 
 import com.app.api.logout.service.LogoutService;
 import com.app.global.util.AuthorizationHeaderUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Tag(name = "authentication", description = "login/logout 토큰 재발급 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class LogoutController {
     private final LogoutService logoutService;
+
+    @Tag(name = "authentication")
+    @Operation(summary = "로그아웃 API", description = "로그아웃 시 refresh token 만료 처리")
     @PostMapping("/logout")
     public ResponseEntity<String> logout(HttpServletRequest httpServletRequest) {
         String authorizationHeader = httpServletRequest.getHeader("Authorization");
